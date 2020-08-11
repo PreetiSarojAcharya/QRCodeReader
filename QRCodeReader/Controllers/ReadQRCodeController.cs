d4818443fb5aaabfb290b630394da093e3a57545
PreetiSarojAcharya <Preeti.saroj@gmail.com>
2020-08-11 20:55:30 +0530

    commit

--- QRCodeReader/Controllers/ReadQRCodeController.cs
+++ QRCodeReader/Controllers/ReadQRCodeController.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web;
-using System.Web.Mvc;
-using System.Xml.Linq;
-using Newtonsoft.Json;
-using System.Dynamic;
-using Newtonsoft.Json.Converters;
+﻿using System.Web.Mvc;
 
 namespace QRCodeReader.Controllers
 {
