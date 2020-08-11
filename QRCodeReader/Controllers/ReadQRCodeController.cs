d7f72d42d561b0958dccf132c73c7b84996499d2
PreetiSarojAcharya <Preeti.saroj@gmail.com>
2020-08-11 19:16:57 +0530

    commit changes

--- QRCodeReader/Controllers/ReadQRCodeController.cs
+++ QRCodeReader/Controllers/ReadQRCodeController.cs
@@ -18,50 +18,8 @@ namespace QRCodeReader.Controllers
         // GET: ReadQRCode
         public ActionResult Index(string jsonReceiverInCsharpObjecName)
         {
-
-          //  dynamic dynData = JsonConvert.DeserializeObject<ExpandoObject>
-          //(jsonReceiverInCsharpObjecName, new ExpandoObjectConverter());
-
-          //  foreach (KeyValuePair<string, object> transItem in dynData)
-          //  {
-
-          //      if (transItem.Key == "transaction_time")
-          //      { }
-
-          //      //else
-          //      //do for rest attribute of your json data
-          //  }
-
-           // return true;
-
-          return View();
+          return View();          
+        }     
              
-        }
-
-
-         private HttpWebRequest GetRequest(string url, string httpMethod = "GET", bool allowAutoRedirect = true)
-        {
-            Uri uri = new Uri(url);
-            HttpWebRequest request = HttpWebRequest.Create(url) as HttpWebRequest;
-            request.UserAgent = @"Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko";
-          
-            request.Timeout = Convert.ToInt32(new TimeSpan(0, 5, 0).TotalMilliseconds);
-                request.Method = httpMethod;
-            return request;
-        }
-
-
-        [HttpPost]
-        public ActionResult ProcessForm( )
-        {
-            HttpClient client = new HttpClient();
-
-            ServicePointManager.ServerCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
-            HttpWebRequest request = this.GetRequest("http://api.qrserver.com/v1/read-qr-code/?fileurl=http%3A%2F%2Fapi.qrserver.com%2Fv1%2Fcreate-qr-code%2F%3Fdata%3DHelloWorld");
-            WebResponse webResponse = request.GetResponse();
-            var responseText = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
-
-            return View(responseText);
-        }
     }
 }
